f766cc857d8532cf37c66e1c24cc1392a6d108f1
Djako Akpe Emmanuel <akpemanou@gmail.com>
2019-07-17 10:54:21 +0000

    AUTHOR: EMMANUEL DJAKO EMAIL: akpemanou@gmail.com
    -Changed Proile Interface

--- app/src/main/java/com/example/alc40phase1/MyWebViewClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.alc40phase1;
-
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-abstract class MyWebViewClient extends WebViewClient {
-    public abstract boolean shouldOverrideLoading(WebView view, String url);
-}
--- app/src/main/java/com/example/alc40phase1/Profile.java
+++ app/src/main/java/com/example/alc40phase1/Profile.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        CircleImageView circleImageView = findViewById(R.id.circle);
+
     }
 }
